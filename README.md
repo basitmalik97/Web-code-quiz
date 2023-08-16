fd123d103bc28f389165ac2063b83d0b7bf98d91
basitmalik97 <basitmalik97@gmail.com>
2023-08-15 21:08:51 -0400

    create JS based quiz application

---
+++ README.md
@@ -0,0 +1,51 @@
+# JavaScript Quiz Application
+
+Welcome to my timed programming quiz application. Please read the following instructions before you begin:
+
+## Instructions
+
+- You have 120 seconds to complete the quiz.
+- You are allowed to skip questions if needed.
+- Once an answer is selected, it cannot be changed.
+- Providing an incorrect answer will result in a 10-second deduction.
+- Each correct answer adds 10 points to your score.
+- Your final score receives a 1-point boost for each remaining second at the end.
+
+## Screenshots
+
+![Quiz Start](assets/screenshots/Start-quiz.png)
+*Quiz Start*
+
+![Quiz Finish](assets/screenshots/Finish-quiz.png)
+*Quiz Finish*
+
+![Highscore](assets/screenshots/Highscore-quiz.png)
+*Highscore*
+
+## Getting Started
+
+To run the quiz application locally, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Open the `index.html` file in a web browser.
+
+## Features
+
+- Timed quiz with a countdown timer.
+- Multiple-choice questions with options.
+- Dynamic scoring system based on correct answers and remaining time.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Contributions
+
+Contributions are welcome! If you'd like to contribute to the project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature/bugfix.
+3. Make your changes and submit a pull request.
+
---
+++ assets/js/questions.js
@@ -0,0 +1,117 @@
+const questions = [
+  
+  {
+    text:
+      "Which keyword is used to declare a variable in JavaScript?",
+    answers: [
+      "A) declare",
+      "B) var",
+      "C) int",
+      "D) variable",
+    ],
+    correct: "B",
+  },
+  {
+    text:
+      "What symbol is used for single-line comments in JavaScript?",
+    answers: ["A) //", 'B) "/* */"', "C) --", 'D) %%'],
+    correct: "A",
+  },
+  {
+    text: "Which of the following is NOT a JavaScript data type?",
+    answers: [
+      "A) string",
+      "B) boolean",
+      "C) number",
+      "D) float",
+    ],
+    correct: "D",
+  },
+  {
+    text:
+      "What is the result of typeof null in JavaScript?",
+    answers: ["A) object", "B) null", "C) undefined", "D) string"],
+    correct: "A",
+  },
+  {
+    text: "What does the '===' operator do in JavaScript?",
+    answers: [
+      "A) Assigns a value to a variable",
+      "B) Compares values for equality, including data type",
+      "C) Concatenates strings",
+      "D) Declares a new function",
+    ],
+    correct: "B",
+  },
+  {
+    text: "Which built-in function is used to print something to the console?",
+    answers: [
+      "A) console.log()",
+      "B) print()",
+      "C) display()",
+      "D) log()",
+    ],
+    correct: "A",
+  },
+  {
+    text: "What is the purpose of the 'if' statement in JavaScript?",
+    answers: [
+      "A) To define a loop",
+      "B) To declare a variable",
+      "C) To create a function",
+      "D) To make a decision based on a condition",
+    ],
+    correct: "D",
+  },
+  {
+    text:
+      "How do you access the length of an array called 'myArray' in JavaScript?",
+    answers: [
+      "A) myArray.size",
+      "B) myArray.length",
+      "C) myArray.count",
+      "D) myArray.total",
+    ],
+    correct: "B",
+  },
+  {
+    text: "What is the correct syntax to create a function in JavaScript?",
+    answers: [
+      "A) function = myFunction()",
+      "B) function myFunction()",
+      "C) myFunction = function()",
+      "D) func myFunction()",
+    ],
+    correct: "B",
+  },
+  {
+    text: "What does HTML stand for?",
+    answers: [
+      "A) Hyper Transfer Markup Language",
+      "B) Hyper Text Markup Language",
+      "C) High Tech Modern Language",
+      "D) Hyperlink and Text Markup Language",
+    ],
+    correct: "B",
+  },
+  {
+    text: "Which CSS property is used to control the spacing between elements?",
+    answers: [
+      "A) margin",
+      "B) padding",
+      "C) space",
+      "D) gap",
+    ],
+    correct: "B",
+  },
+  {
+    text: "Which HTML tag is used to create a line break?",
+    answers: [
+      "A) <br>",
+      "B) <lb>",
+      "C) <newline>",
+      "D) <ln>",
+    ],
+    correct: "A",
+  },
+];
